af5c75f380a6ab18b86e6501130faf60ed0e5cc0
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2018-09-29 21:46:17 -0600

    Rename folder

---
+++ intro/code_spec.js
@@ -0,0 +1,8 @@
+// import {vars} from './code';
+const expect = require('chai').expect;
+
+describe('Javascript principles', function() {
+  it('can define an integer variable', function() {
+    expect(true).should.equal(true);
+  });
+});
---
+++ intro/variables_spec.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+const code = require;
+
+describe('Variables', function() {
+  it('can define an integer variable', function() {
+    expect(true).to.equal(true);
+  });
+});
