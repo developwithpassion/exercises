80fee05f981edbf7af604e48ddc2a9e76dbf9739
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2018-10-01 11:30:26 -0600

    Switch to intro

--- intro/code_spec.js
+++
@@ -1,8 +0,0 @@
-// import {vars} from './code';
-const expect = require('chai').expect;
-
-describe('Javascript principles', function() {
-  it('can define an integer variable', function() {
-    expect(true).should.equal(true);
-  });
-});
--- intro/variables_spec.js
+++
@@ -1,8 +0,0 @@
-const expect = require('chai').expect;
-const code = require;
-
-describe('Variables', function() {
-  it('can define an integer variable', function() {
-    expect(true).to.equal(true);
-  });
-});
